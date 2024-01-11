a2af1aa8175c21573682ea15f6c6abae2d6777c7
timattt <timattt@gmail.com>
2024-01-11 18:27:55 +0300

    scheduled ping

--- src/main/java/org/shlimtech/typesix/debug/DebugConfig.java
+++ src/main/java/org/shlimtech/typesix/debug/DebugConfig.java
@@ -2,21 +2,27 @@ package org.shlimtech.typesix.debug;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.shlimtech.typesix.dto.UserDTO;
 import org.shlimtech.typesix.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @Profile("debug")
 @RequiredArgsConstructor
+@EnableScheduling
+@Log
 public class DebugConfig {
 
     private final UserService userService;
@@ -33,6 +39,12 @@ public class DebugConfig {
             UserDTO userDTO = userService.loadUser(email);
             context.getClaims().claim("email", userDTO.getEmail()).claim("id", userDTO.getId());
         };
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void testType6d() {
+        RestTemplate restTemplate = new RestTemplate();
+        log.info(restTemplate.getForEntity("http://10.96.169.230:443/task", String.class).toString());
     }
 
     @Bean
