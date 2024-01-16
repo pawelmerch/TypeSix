91ca80c721e2ea46a06a9671beab657b19012cab
timattt <timattt@gmail.com>
2024-01-16 20:52:02 +0300

    remove unused and fix bean scanning

--- TypeSixCore/src/main/java/org/shlimtech/typesix/TypeSixApplication.java
+++ TypeSixCore/src/main/java/org/shlimtech/typesix/TypeSixApplication.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "org.shlimtech.typesixdatabasecommon")
-@ComponentScan(basePackages = "org.shlimtech.typesixdatabasecommon")
-@EntityScan(basePackages = "org.shlimtech.typesixdatabasecommon")
+@EnableJpaRepositories(basePackages = "org.shlimtech")
+@ComponentScan(basePackages = "org.shlimtech")
+@EntityScan(basePackages = "org.shlimtech")
 public class TypeSixApplication {
 
     public static void main(String[] args) {
