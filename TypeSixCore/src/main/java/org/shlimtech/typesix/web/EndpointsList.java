5f3a586df47b8556a050468eec0cdb4987b7a91a
timattt <timattt@gmail.com>
2024-06-02 12:35:08 +0300

    resolving timattt/TypeSixCloud#9
    changed prefix

--- TypeSixCore/src/main/java/org/shlimtech/typesix/web/EndpointsList.java
+++ TypeSixCore/src/main/java/org/shlimtech/typesix/web/EndpointsList.java
@@ -40,9 +40,9 @@ import static org.shlimtech.typesix.web.swagger.Type6Endpoint.ENDPOINT_CATEGORY.
  * <br/>
  */
 public class EndpointsList {
-    public static final String PREFIX = "/sso";
+    public static final String PREFIX_PUBLIC = "/public";
     public static final String PREFIX_INTERNAL = "";
-    public static final String PREFIX_PAGE = PREFIX + "/pages";
+    public static final String PREFIX_PUBLIC_PAGE = PREFIX_PUBLIC + "/pages";
 
 
     //
@@ -54,14 +54,14 @@ public class EndpointsList {
             method = POST,
             category = PUBLIC
     )
-    public static final String OAUTH2_TOKEN_ENDPOINT = PREFIX + "/oauth2/token";
+    public static final String OAUTH2_TOKEN_ENDPOINT = PREFIX_PUBLIC + "/oauth2/token";
 
     @Type6Endpoint(
             description = "Сюда клиент переходит для начала внешнего конвейера oauth2",
             method = GET,
             category = PUBLIC
     )
-    public static final String OAUTH2_AUTHORIZATION_ENDPOINT = PREFIX + "/oauth2/authorize";
+    public static final String OAUTH2_AUTHORIZATION_ENDPOINT = PREFIX_PUBLIC + "/oauth2/authorize";
 
     @Type6Endpoint(
             description = "Сюда другие микросервисы подключаются, чтобы получить JWK",
@@ -75,14 +75,14 @@ public class EndpointsList {
             method = POST,
             category = PUBLIC
     )
-    public static final String TOKEN_INTROSPECTION_ENDPOINT = PREFIX + "/oauth2/introspect";
+    public static final String TOKEN_INTROSPECTION_ENDPOINT = PREFIX_PUBLIC + "/oauth2/introspect";
 
     @Type6Endpoint(
             description = "Путь для завершения сессии",
             method = GET,
             category = PUBLIC
     )
-    public static final String LOGOUT_ENDPOINT = PREFIX + "/logout";
+    public static final String LOGOUT_ENDPOINT = PREFIX_PUBLIC + "/logout";
 
 
     //
@@ -93,14 +93,14 @@ public class EndpointsList {
             method = GET,
             category = THIRD_PARTY
     )
-    public static final String THIRD_PARTY_AUTHORIZATION_ENDPOINT = PREFIX + "/oauth2/authorization";
+    public static final String THIRD_PARTY_AUTHORIZATION_ENDPOINT = PREFIX_PUBLIC + "/oauth2/authorization";
 
     @Type6Endpoint(
             description = "Внешний провайдер по протоколу oauth2 возвращается сюда с кодом",
             method = GET,
             category = THIRD_PARTY
     )
-    public static final String THIRD_PARTY_CODE_ENDPOINT = PREFIX + "/login/oauth2/code";
+    public static final String THIRD_PARTY_CODE_ENDPOINT = PREFIX_PUBLIC + "/login/oauth2/code";
 
 
     //
@@ -111,42 +111,42 @@ public class EndpointsList {
             method = POST,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_EMAIL_ENDPOINT = PREFIX + "/registration/email";
+    public static final String REGISTRATION_EMAIL_ENDPOINT = PREFIX_PUBLIC + "/registration/email";
 
     @Type6Endpoint(
             description = "Второй шаг конвейера",
             method = POST,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_CODE_ENDPOINT = PREFIX + "/registration/verify";
+    public static final String REGISTRATION_CODE_ENDPOINT = PREFIX_PUBLIC + "/registration/verify";
 
     @Type6Endpoint(
             description = "Третий шаг конвейера",
             method = POST,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_PASSWORD_SET_ENDPOINT = PREFIX + "/registration/password";
+    public static final String REGISTRATION_PASSWORD_SET_ENDPOINT = PREFIX_PUBLIC + "/registration/password";
 
     @Type6Endpoint(
             description = "Страница для первого шага конвейера",
             method = GET,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_EMAIL_PAGE = PREFIX_PAGE + "/registration/email";
+    public static final String REGISTRATION_EMAIL_PAGE = PREFIX_PUBLIC_PAGE + "/registration/email";
 
     @Type6Endpoint(
             description = "Страница для второго шага конвейера",
             method = GET,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_CODE_PAGE = PREFIX_PAGE + "/registration/verify";
+    public static final String REGISTRATION_CODE_PAGE = PREFIX_PUBLIC_PAGE + "/registration/verify";
 
     @Type6Endpoint(
             description = "Страница для третьего шага конвейера",
             method = GET,
             category = REGISTRATION
     )
-    public static final String REGISTRATION_PASSWORD_SET_PAGE = PREFIX_PAGE + "/registration/password";
+    public static final String REGISTRATION_PASSWORD_SET_PAGE = PREFIX_PUBLIC_PAGE + "/registration/password";
 
 
     //
@@ -157,21 +157,21 @@ public class EndpointsList {
             method = GET,
             category = PAGE
     )
-    public static final String LOGIN_PAGE = PREFIX_PAGE + "/login";
+    public static final String LOGIN_PAGE = PREFIX_PUBLIC_PAGE + "/login";
 
     @Type6Endpoint(
             description = "Сюда будет переадресация после успешного логина, при отсутсвии привязки к oauth2-клиенту",
             method = GET,
             category = PAGE
     )
-    public static final String SUCCESS_LOGIN_PAGE = PREFIX_PAGE + "/success";
+    public static final String SUCCESS_LOGIN_PAGE = PREFIX_PUBLIC_PAGE + "/success";
 
     @Type6Endpoint(
             description = "Сюда перенаправляются ошибки",
             method = GET,
             category = PAGE
     )
-    public static final String ERROR_PAGE = PREFIX_PAGE + "/error";
+    public static final String ERROR_PAGE = PREFIX_PUBLIC_PAGE + "/error";
 
 
     //
@@ -189,19 +189,19 @@ public class EndpointsList {
             method = POST,
             category = OTHER
     )
-    public static final String FORM_LOGIN_ENDPOINT = PREFIX + "/formlogin";
+    public static final String FORM_LOGIN_ENDPOINT = PREFIX_PUBLIC + "/formlogin";
 
     @Type6Endpoint(
             description = "Путь для интерфейста SWAGGER",
             method = GET,
             category = OTHER
     )
-    public static final String SWAGGER_UI_BASE_PATH = "/docs/swagger-ui";
+    public static final String SWAGGER_UI_BASE_PATH = PREFIX_INTERNAL + "/docs/swagger-ui";
 
     @Type6Endpoint(
             description = "Путь для эндпоинта с документацией",
             method = GET,
             category = OTHER
     )
-    public static final String SPRING_DOC_PATH = "/docs";
+    public static final String SPRING_DOC_PATH = PREFIX_INTERNAL + "/docs";
 }
