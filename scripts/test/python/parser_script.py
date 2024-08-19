f26669307a1e2cc4c2f8cc62eeab32f728a1302f
timattt <timattt@gmail.com>
2024-08-19 18:54:25 +0300

    resolving timattt/TypeSix#24
    1. half ok response code counting
    2. bigger tests

--- scripts/test/python/parser_script.py
+++ scripts/test/python/parser_script.py
@@ -2,6 +2,12 @@ import csv
 import sys
 import datetime
 
+print("RAMP_TEST_ANALYSE")
+
+#
+# Говнокод для вычисления RPS.
+#
+
 with open(sys.argv[1], newline='') as csvfile:
     spam_reader = csv.reader(csvfile)
 
@@ -18,8 +24,14 @@ with open(sys.argv[1], newline='') as csvfile:
     code_2xx_3xx_per_second = dict()
     code_other_per_second = dict()
 
+    total_time = 0
+
     for timestamp, code in zip(timestamps, responseCodes):
-        code = int(code)
+        total_time += timestamp
+        try:
+            code = int(code)
+        except ValueError:
+            code = 0
         if 200 <= code and code < 400:
             if timestamp in code_2xx_3xx_per_second:
                 code_2xx_3xx_per_second[timestamp] += 1
@@ -31,17 +43,23 @@ with open(sys.argv[1], newline='') as csvfile:
             else:
                 code_other_per_second[timestamp] = 1
 
+    avg_time = total_time / len(timestamps)
     total_ok_counts = 0
+    total_half_ok_counts = 0
+    total_half_ok_counts_count = 0
 
     for second, count in code_2xx_3xx_per_second.items():
         total_ok_counts += count
+        if avg_time <= second:
+            total_half_ok_counts += count
+            total_half_ok_counts_count += 1
 
     total_bad_counts = 0
 
     for second, count in code_other_per_second.items():
-        total_ok_counts += count
+        total_bad_counts += count
 
-    rps = total_ok_counts / len(code_2xx_3xx_per_second)
+    rps = total_half_ok_counts / total_half_ok_counts_count
     rps = int(rps)
 
     message = '{} Ramp-test. RPS = {}, Total ok requests = {}, Total bad requests = {}'.format(datetime.datetime.now(), rps, total_ok_counts, total_bad_counts)
