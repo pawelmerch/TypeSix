0dba531a9c9488b33a8dabc34c27d9cd509bddd6
timattt <timattt@gmail.com>
2024-08-22 13:11:07 +0300

    resolving timattt/TypeSix#26
    1. rename advice
    2. add docker compose name

--- TypeSixCore/src/main/java/io/mipt/typesix/core/web/controller/CustomControllerAdvice.java
+++ TypeSixCore/src/main/java/io/mipt/typesix/core/web/controller/CustomControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class Advice {
+public class CustomControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<String> errorHandler(Exception exception) {
         return ResponseEntity.internalServerError().body(exception.getMessage());
